9029415144c68310094c9ad3536666b49f0325dc
Patrik Horvath <xhorvathp@stuba.sk>
2026-06-04 22:49:43 +0200

    H-mostík bol nepotrebný, zmena zapojenia, prijímanie príkazov zo Serial
    Tesované a písané spolu s Haiom, H-mostík bol používaný na beh motora, toto riešenie nebolo dobré a niekedy sa motor zastavil, nedostatok výkonu. Motor bol zapojený napriamo na 5V 2A zdroj a beží stále. PI regulátor z pozorovania funguje dostatočne dobre.

--- ArduinoPELTIER.ino
+++ ArduinoPELTIER.ino
@@ -1,12 +1,7 @@
 #include <OneWire.h>
 #include <DallasTemperature.h>
 
-const int PUMP_ENB = 6;  
-const int PUMP_IN3 = 8;  
-const int PUMP_IN4 = 9;  
-
 const int PELTIER = 5;   
-
 const int TEMP_PIN = 2;  
 
 OneWire oneWire(TEMP_PIN);
@@ -14,36 +9,75 @@ DallasTemperature sensors(&oneWire);
 
 int peltierPWM = 0;
 int pwmPercent = 0;
-bool isPumpRunning = false;
 bool isSystemActive = false;
 bool debugActive = false;
 unsigned long lastTempCheck = 0;
 
+String inputBuffer = "";
+
+float setpoint = 20.0;
+float integralSum = 0.0;
+
+const float Kp = 30;
+const float Ki = 10; 
+const float Ts = 2.0;
+
 void setup() {
-  pinMode(PUMP_ENB, OUTPUT);
-  pinMode(PUMP_IN3, OUTPUT);
-  pinMode(PUMP_IN4, OUTPUT);
   pinMode(PELTIER, OUTPUT);
-
-  digitalWrite(PUMP_IN3, HIGH);
-  digitalWrite(PUMP_IN4, LOW);
-
-  analogWrite(PUMP_ENB, 0);
   analogWrite(PELTIER, 0);
 
   Serial.begin(115200);
+  inputBuffer.reserve(32);
   
   sensors.begin();
   stopAll();
+  
+  Serial.println("ARDUINO_READY");
 }
 
 void loop() {
+  while (Serial.available() > 0) {
+    char inChar = (char)Serial.read();
+    if (inChar == '\n' || inChar == '\r') {
+      if (inputBuffer.length() > 0) {
+        processCommand(inputBuffer);
+        inputBuffer = "";
+      }
+    } else {
+      inputBuffer += inChar;
+    }
+  }
+
+  if (debugActive) {
+    analogWrite(PELTIER, peltierPWM);
+  }
+
   if (isSystemActive && (millis() - lastTempCheck >= 2000)) {
     sensors.requestTemperatures();
     float celsius = sensors.getTempCByIndex(0);
     
-    if (celsius != -127.00) {
-      // int pwmPercent = map(peltierPWM, 0, 255, 0, 100);
+    if (celsius != DEVICE_DISCONNECTED_C) {
+      if (isSystemActive && !debugActive) {
+        float error = celsius - setpoint;
+
+        if (error > 0.5) {
+          pwmPercent = 100;
+          integralSum = 0.0;
+        } else if (error < -0.5) {
+          pwmPercent = 0;
+          integralSum = 0.0;
+        } else {
+          integralSum += error * Ts;
+          integralSum = constrain(integralSum, 0.0, 100.0 / Ki);
+
+          float output = (Kp * error) + (Ki * integralSum);
+          pwmPercent = constrain((int)output, 0, 100);
+        }
+
+        peltierPWM = map(pwmPercent, 0, 100, 0, 255);
+        analogWrite(PELTIER, peltierPWM);
+      }
+
       Serial.print("DATA:");
       Serial.print(celsius);
       Serial.print(",");
@@ -51,45 +85,36 @@ void loop() {
     }
     lastTempCheck = millis();
   }
-
-  if (Serial.available() > 0) {
-    String inputString = Serial.readStringUntil('\n');
-    inputString.trim();
-    
-    if (inputString == "ON") {
-      coolMode();
-    } 
-    else if (inputString == "OFF") {
-      stopAll();
-    } 
-    else if (inputString.startsWith("DEBUG:")) {
-      int val = inputString.substring(6).toInt();
-      debugActive = (val == 1);
-    }
-    else if (inputString.startsWith("PWM:")) {
-      if (debugActive) {
-        pwmPercent = inputString.substring(4).toInt();
-        pwmPercent = constrain(pwmPercent, 0, 100);
-        peltierPWM = map(pwmPercent, 0, 100, 0, 255);
-        analogWrite(PELTIER, peltierPWM);
-      }
-    }
-  }
 }
 
-void coolMode() {
-  isSystemActive = true;
-  if (!isPumpRunning) {
-    analogWrite(PUMP_ENB, 255); 
-    delay(150);                 
-    isPumpRunning = true;
+void processCommand(String cmd) {
+  cmd.trim();
+  
+  if (cmd == "ON") {
+    isSystemActive = true;
+  } 
+  else if (cmd == "OFF") {
+    isSystemActive = false;
+    stopAll();
+  } 
+  else if (cmd.startsWith("DEBUG:")) {
+    int val = cmd.substring(6).toInt();
+    debugActive = (val == 1);
   }
-  analogWrite(PELTIER, peltierPWM);  
+  else if (cmd.startsWith("PWM:")) {
+    pwmPercent = cmd.substring(4).toInt();
+    pwmPercent = constrain(pwmPercent, 0, 100);
+    peltierPWM = map(pwmPercent, 0, 100, 0, 255);
+  }
+  else if (cmd.startsWith("SP:")) {
+    setpoint = cmd.substring(3).toFloat();
+  }
 }
 
 void stopAll() {
-  analogWrite(PELTIER, 0);
-  analogWrite(PUMP_ENB, 0);
-  isPumpRunning = false;
   isSystemActive = false;
+  peltierPWM = 0;
+  pwmPercent = 0;
+  integralSum = 0.0;
+  analogWrite(PELTIER, 0);
 }
